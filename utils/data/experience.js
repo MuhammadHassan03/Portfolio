36337bb4ff14c84a8aae168e8f65c8be6e50e1c9
Muhammad Hassan <97297671+MuhammadHassan03@users.noreply.github.com>
2024-10-30 01:10:39 +0500

    added new experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Engineer Intern,
+    company: "Devellp",
+    duration: "(October 2024 - Present)"
+  },
+  {
+    id: 2,
     title: 'React Native Developer',
     company: "Bierman Collective",
     duration: "(Feburary 2024 - Present)"
   },
   {
-    id: 2,
+    id: 3,
     title: "React Js Internship",
     company: "Internee.pk",
     duration: "(Sep 2023 - Oct 2023)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Aug 2021 - Present)"
+    company: "Code and build something everyday.",
+    duration: "(Aug 2021 - Feb 2024)"
   }
 ]
