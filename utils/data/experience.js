c724e3792e16fb352d057c8fafb1a644d9e26a3f
Muhammad Hassan <97297671+MuhammadHassan03@users.noreply.github.com>
2025-02-12 10:04:08 +0000

    Experience Updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,24 +1,30 @@
 export const experiences = [
   {
     id: 1,
-    title: "Software Engineer Intern",
+    title: "Software Engineer",
     company: "Devellp",
-    duration: "(October 2024 - Present)"
+    duration: "(December 2024 - Present)"
   },
   {
     id: 2,
+    title: "Software Engineer Intern",
+    company: "Devellp",
+    duration: "(October 2024 - December 2024)"
+  },
+  {
+    id: 3,
     title: "React Native Developer",
     company: "Bierman Collective",
     duration: "(Feburary 2024 - Present)"
   },
   {
-    id: 3,
+    id: 4,
     title: "React Js Internship",
     company: "Internee.pk",
     duration: "(Sep 2023 - Oct 2023)"
   },
   {
-    id: 4,
+    id: 5,
     title: "Self Employed",
     company: "Code and build something everyday.",
     duration: "(Aug 2021 - Feb 2024)"
